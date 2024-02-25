4cd61493f738184c66fa4fc363b5940936385f16
Ekaterina Soloveva <9179885@gmail.com>
2024-02-25 20:37:02 +0300

    Task 2

--- hw-5.js
+++ hw-5.js
@@ -6,3 +6,13 @@ return a < b ? a : b ;
 console.log(min(8, 4));
 console.log(min(6, 6));
 
+//-------------Task 2-------------------------
+
+let number = Number(prompt ('Введите любое число'));
+
+let isEven = (number) => {
+    return number % 2 === 0 ? 'Число четное': 'Нечетное число';
+}
+
+console.log(isEven(number));
+
