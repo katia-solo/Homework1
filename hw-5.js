7a71fe4b2ad649fe3e08dcc661e9f8f8b65fe5ff
Ekaterina Soloveva <9179885@gmail.com>
2024-02-25 21:11:01 +0300

    Task 4

--- hw-5.js
+++ hw-5.js
@@ -1,29 +1,44 @@
 //-------------Task 1-------------------------
 
 let min = (a, b) => {
-return a < b ? a : b ;
+    return a < b ? a : b;
 }
 console.log(min(8, 4));
 console.log(min(6, 6));
 
 //-------------Task 2-------------------------
 
-let number = Number(prompt ('Введите любое число'));
+let number = Number(prompt('Введите любое число'));
 
 let isEven = (number) => {
-    return number % 2 === 0 ? 'Число четное': 'Нечетное число';
+    return number % 2 === 0 ? 'Число четное' : 'Нечетное число';
 }
 
 console.log(isEven(number));
 
 //-------------Task 3-------------------------
 
-let squaerNumber = (n) => n*=n;
+let squaerNumber = (n) => n *= n;
 console.log(squaerNumber(5));
 
 let squaerNumber2 = (n) => {
-    let result = n*n;
+    let result = n * n;
     return result;
 }
 console.log(squaerNumber2(5));
+
+//-------------Task 4-------------------------
+
+let age = Number(prompt('Сколько вам лет?'));
+
+let ageCheck = (age) => {
+    if (isNaN(age) || Math.sign(age) === -1) {
+        return 'Вы ввели неправильное значение';
+    } else if (age > 0 && age <= 12) {
+        return 'Привет, друг!';
+    } {
+        return 'Добро пожаловать!';
+    }
+}
+alert(ageCheck(age));
 
