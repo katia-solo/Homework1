1694c38cd981305c2acfdd7d2e30babcf9905321
Ekaterina Soloveva <9179885@gmail.com>
2024-03-03 18:59:38 +0300

    task 1

---
+++ hw-6.js
@@ -0,0 +1,9 @@
+//-------------------Task 1--------------------------
+
+const numbers = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < numbers.length; i++) {
+   if (numbers[i] == 0) break;
+   console.log(numbers[i]);
+}
+
