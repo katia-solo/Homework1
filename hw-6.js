c4e9ae018f21ac4039a941fc1ea56382aac29cf6
Ekaterina Soloveva <9179885@gmail.com>
2024-03-03 19:51:55 +0300

    Task 5

--- hw-6.js
+++ hw-6.js
@@ -16,6 +16,26 @@ console.log(`4 is located on position number ${position}`);
 
 //-------------------Task 3--------------------------
 
-let mass = [1, 3, 5, 10, 20];
+const mass = [1, 3, 5, 10, 20];
 console.log(mass.join(' '));
 
+//-------------------Task 4--------------------------
+
+const multiArray = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+//вариант 1
+// for (let i = 0; i < multiArray.length; i++) {
+//     // Сначала выводим в консоль индекс подмассива
+//     console.log(`Подмассив ${i}:`);
+//     for (let j = 0; j < multiArray[i].length; j++) {
+//         // Затем выводим каждый элемент подмассива
+//         console.log(multiArray[i][j]);
+//     }
+// }
+//вариант 2
+console.log(JSON.stringify(multiArray));
+
+//-------------------Task 5--------------------------
+
+const addingElements = [1, 1, 1];
+addingElements.push(2, 2, 2);
+console.log(addingElements);
