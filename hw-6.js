25d895ee978d2e33b1d5498fe123398e30eea73c
Ekaterina Soloveva <9179885@gmail.com>
2024-03-03 19:04:13 +0300

    Task 1 fixed

--- hw-6.js
+++ hw-6.js
@@ -3,7 +3,9 @@
 const numbers = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < numbers.length; i++) {
-   if (numbers[i] == 0) break;
-   console.log(numbers[i]);
+    console.log(numbers[i]);
+    if (numbers[i] === 10) break;
 }
+
+//-------------------Task 2--------------------------
 
