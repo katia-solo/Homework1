00d912f0a6bfd05762a164a0698a24bb60d35d0b
Ekaterina Soloveva <9179885@gmail.com>
2024-03-10 22:13:30 +0300

    Task 4

--- hw-7.js
+++ hw-7.js
@@ -23,3 +23,9 @@ let num = 32.58884;
 console.log(`Меньшее целое: ${Math.floor(num)}`);
 console.log(`Большее целое: ${Math.ceil(num)}`);
 console.log(`Ближайшее целое: ${Math.round(num)}`);
+
+//--------------Task 4--------------------
+const arr = [52, 53, 49, 77, 21, 32];
+
+console.log(`Максимальное: ${Math.max(...arr)}`);
+console.log(`Минимальное: ${Math.min(...arr)}`);
