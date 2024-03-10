85c8f203ad2024ff6411e84fab7692832f2992fa
Ekaterina Soloveva <9179885@gmail.com>
2024-03-10 22:23:30 +0300

    Task 5

--- hw-7.js
+++ hw-7.js
@@ -29,3 +29,10 @@ const arr = [52, 53, 49, 77, 21, 32];
 
 console.log(`Максимальное: ${Math.max(...arr)}`);
 console.log(`Минимальное: ${Math.min(...arr)}`);
+
+//--------------Task 5--------------------
+function randomNumberFrom1To10() {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    console.log(`Случайное число от 1 до 10: ${randomNumber}`);
+}
+randomNumberFrom1To10();
