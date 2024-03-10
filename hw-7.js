aeb7313d5c85594c9cca0ad8982cf4b703901c7b
Ekaterina Soloveva <9179885@gmail.com>
2024-03-10 22:02:52 +0300

    Task 2

--- hw-7.js
+++ hw-7.js
@@ -1,3 +1,18 @@
 //--------------Task 1--------------------
 let str = 'js';
 console.log(str.toUpperCase());
+
+//--------------Task 2--------------------
+function filterByStartsWith(array, startString) {
+
+    // Фильтруем массив
+    const filteredArray = array.filter(item => item.toLowerCase().startsWith(startString.toLowerCase()));
+
+    return filteredArray;
+}
+
+// Пример использования функции
+const array = ["apple", "banana", "Orange", "pineapple", "oracl"];
+const startString = "or";
+
+console.log(filterByStartsWith(array, startString)); // Выведет: ["Orange"]
