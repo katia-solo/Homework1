77a5a23b731ec44a42e85766d679db8d468f3a26
Ekaterina Soloveva <9179885@gmail.com>
2024-03-16 21:47:06 +0300

    done

--- hw-8.js
+++ hw-8.js
@@ -54,33 +54,33 @@ function printCurrentDate() {
 
 printCurrentDate();
 
-// //---------------------Task 4-----------------
-// function delayForSecond(callback) {
-//     // Код писать можно только внутри этой функции
-//     // callback();
-//     setTimeout(() => callback(), 1000);
-// }
+//---------------------Task 4-----------------
+function delayForSecond(callback) {
+    // Код писать можно только внутри этой функции
+    // callback();
+    setTimeout(() => callback(), 1000);
+}
 
-// delayForSecond(function () {
-//     console.log('Привет, Глеб!');
-// })
+delayForSecond(function () {
+    console.log('Привет, Глеб!');
+})
 
-// //---------------------Task 5-----------------
-// // Функция delayForSecond через 1 секунду пишет в консоль 
-// // «Прошла одна секунда», а затем вызывает переданный колбэк
-// function delayForSecond(cb) {
-//     setTimeout(() => {
-//         console.log('Прошла одна секунда');
-//         if (cb) { cb(); }
-//     }, 1000)
-// }
+//---------------------Task 5-----------------
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
+}
 
-// // Функция sayHi выводит в консоль приветствие для указанного имени
-// function sayHi(name) {
-//     console.log(`Привет, ${name}!`);
-// }
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
 
-// // Код выше менять нельзя
+// Код выше менять нельзя
 
-// // Нужно изменить код ниже:
-// delayForSecond(() => sayHi('Глеб'))
+// Нужно изменить код ниже:
+delayForSecond(() => sayHi('Глеб'))
